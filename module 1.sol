7affab7fefff84b079c261a990d7e19525592e9e
harsh08hc <136369712+harsh08hc@users.noreply.github.com>
2023-07-06 02:07:09 +0530

    module 1.sol

--- module 1.sol
+++ module 1.sol
@@ -1,29 +1,22 @@
- // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
-contract Errorhandling {
-    bool public sunny = true;
-    bool public umbrella = false;
-    uint finalCall = 0;
-
-    
-    function weather () public {
-        require(sunny,"IT IS NOT SUNNY TODAY!!");
-        finalCall += 3;
-    
-        assert (finalCall != 0);
+contract HandlingOfError {
+    uint b=10;
+    // assert function
+    function Assert(uint num) public pure{
+        assert(num!=0);
     }
-    function weatherChanger() public{
-        sunny = !sunny;
-    }
-    function getCal() public view returns(uint) {
-        return finalCall;
-    }
-    function BringUmbrella () public {
-        if(!sunny){
-            umbrella = true;
-        }else{
-            revert("No Umbrella needed");
+    // revert function
+    function divideRevert(uint _numerator, uint _denomenator) public pure returns (uint){
+        if(_numerator<_denomenator){  
+            revert("numerator should be greater than denomenator according to condition");        
         }
+        return _numerator/_denomenator;
+    }
+    //require function
+    function multiplicationRequire(uint a) public view returns (uint){
+        require(a>0,"Value of a is zero , as expecting result should not be zero");
+        return a*b;
     }
 }
